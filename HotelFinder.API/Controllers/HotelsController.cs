561e86dfc698a4978bc1684bf2bdd6ed5807f9d4
Doğan Kartal <dogankartal75@gmail.com>
2021-05-18 00:52:11 +0300

    Add project files.

---
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -0,0 +1,107 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelFinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private IHotelService _hotelService;
+
+        public HotelsController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        /// <summary>
+        /// Get All Hotels
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var hotels= await _hotelService.GetAllHotels();
+            return Ok(hotels);
+        }
+
+        /// <summary>
+        /// Get Hotel by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("[action]/{id}")]
+        public async Task<IActionResult> GetHotelById(int id)
+        {
+            var hotel= await _hotelService.GetHotelById(id);
+            if (hotel!=null)
+            {
+                return Ok(hotel);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Route("[action]/{name}")]
+        public async Task<IActionResult> GetHotelByName(string name)
+        {
+            var hotel = await _hotelService.GetHotelByName(name);
+            if (hotel!=null)
+            {
+                return Ok(hotel);
+            }
+
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Create a Hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> CreateHotel([FromBody]Hotel hotel)
+        {
+            var createdHotel= await _hotelService.CreateHotel(hotel);
+            return CreatedAtAction("Get", new { id = createdHotel.Id }, createdHotel);
+        }
+
+        /// <summary>
+        /// Update the Hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> UpdateHotel([FromBody] Hotel hotel)
+        {
+            if (await _hotelService.GetHotelById(hotel.Id)!=null)
+            {
+                return Ok(await _hotelService.UpdateHotel(hotel));
+            }
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Delete the Hotel
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete]
+        [Route("[action]/{id}")]
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            if (await _hotelService.GetHotelById(id)!=null)
+            {
+                await _hotelService.DeleteHotel(id);
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
